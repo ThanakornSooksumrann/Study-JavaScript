2567b6aba38e7f3acd733b046258a55842d496a1
ThanakornSooksumrann <Thanakorn.sook12@gmail.com>
2021-01-20 16:28:52 +0700

    update

---
+++ Lec_3/CreateArrayof.js
@@ -0,0 +1,10 @@
+let arrayLength = 7;
+let arr1 = [];
+let arr2 = Array(arrayLength);
+let arr3 = ['A', false, 5]
+
+console.log(arr1.length);
+console.log(arr2.length);
+console.log(arr3.length);
+console.log(arr3[0]);
+console.log(arr3[2]);
